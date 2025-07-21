003fa13b90ed32826254b01daa7241947da0e0ec
Chandrika9906 <chandrika992006@gmail.com>
2025-07-21 23:02:48 +0530

    Added first 6 pattern programs

---
+++ PatternX.java
@@ -0,0 +1,5 @@
+public class PatternX {
+    public static void main(String[] args) {
+        // write your logic here
+    }
+}
---
+++ Patterns/Pattern1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+/*Enter value of n: 7
+* * * * * * * 
+* * * * * * * 
+* * * * * * *
+* * * * * * *
+* * * * * * *
+* * * * * * *
+* * * * * * * */
